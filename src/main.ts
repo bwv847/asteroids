3b36969d4fafda7ab13067d1871cdeaa31664ad6
Smoliar, Volodymyr <w.smolyar@gmail.com>
2024-08-02 18:36:17 +0400

    Setup canvas, context, canvas styles

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
 import './style.css'
 
+const canvas = document.querySelector('#asteroids-canvas') as HTMLCanvasElement;
+canvas.style.width = window.innerWidth - 50 + 'px';
+canvas.width = canvas.offsetWidth - 50;
+
+canvas.style.height = window.innerHeight - 50 + 'px';
+canvas.height = canvas.offsetHeight - 50;
+
+const context = canvas.getContext('2d');
+
+context.fillRect(20, 0, 100, 100);
